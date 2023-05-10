8a38f2a5e447f11b2fa3578ce7f214a3a1bdd7e8
Miguel Ángel Bernal Valdéz <dm.mbernal@gmail.com>
2023-05-09 20:02:29 -0400

    Javascript

---
+++ js/js/index.js
@@ -0,0 +1,24 @@
+// Sentencia
+console.log("Hola Mundo")
+console.log("Adios Mundo")
+let firstName = "Josemaria" // string
+let lastName = "Gonzalez"
+let edad = 29 // integer number
+let nota = 4.5 // float decimal
+let estudia = true // false boolean
+console.log(firstName)
+console.log(lastName)
+console.log(edad)
+console.log(nota)
+console.log(estudia)
+console.log(firstName + " " + lastName) // concatenacion
+//     funcion
+//           () ejecute
+//            parametros 
+edad = prompt("Que edad tienes?")
+console.log(edad)
+if (edad >= 18) {
+    console.log("Eres mayor de edad")
+} else {
+    console.log("Eres menor de edad")
+}
