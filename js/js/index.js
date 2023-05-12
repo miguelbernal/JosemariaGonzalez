1242fcf02328ac41f0a6ffb9fee795aadf5447e8
Miguel Ángel Bernal Valdéz <dm.mbernal@gmail.com>
2023-05-11 20:03:07 -0400

    Estructura If

--- js/js/index.js
+++ js/js/index.js
@@ -15,10 +15,79 @@ console.log(firstName + " " + lastName) // concatenacion
 //     funcion
 //           () ejecute
 //            parametros 
-edad = prompt("Que edad tienes?")
+//edad = prompt("Que edad tienes?")
 console.log(edad)
 if (edad >= 18) {
     console.log("Eres mayor de edad")
 } else {
     console.log("Eres menor de edad")
 }
+/***********************************************************************/
+// If anidado (No se recomienda usar, es muy confuso)
+var dia = 4
+if (dia === 1){
+    console.log('Domingo')
+} else {
+    if (dia === 2){
+        console.log('Lunes')
+    } else {
+        if (dia === 3){
+            console.log('Martes')
+        } else {
+            if (dia === 4){
+                console.log('Miercoles')
+            }
+        }
+
+    }
+}
+// if else if
+dia = 1
+if (dia === 1){
+    console.log('Domingo')
+} else if (dia === 2){
+    console.log('Lunes')
+} else if (dia === 3){
+    console.log('Martes')
+} else if (dia === 4){
+    console.log('Miercoles')
+} else if (dia === 5){
+    console.log('Jueves')
+} else if (dia === 6){
+    console.log('Viernes')
+} else if (dia === 7){
+    console.log('Sábado')
+} else {
+    console.log('Dia erroneo')
+}
+
+// switch
+console.log('----- switch -----')
+dia = 10
+switch (dia) {
+    case 1:
+        console.log('Domingo')
+        break;
+    case 2:
+        console.log('Lunes')
+        break;
+    case 3:
+        console.log('Martes')
+        break;
+    case 4:
+        console.log('Miercoles')
+        break;
+    case 5:
+        console.log('Jueves')
+        break;
+    case 6:
+        console.log('Viernes')
+        break;
+    case 7:
+        console.log('Sábado')
+        break;
+    default:
+        console.log('Día erróneo')
+        break;
+}
+console.log('Continuando con el programa...')
