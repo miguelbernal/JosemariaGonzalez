c6857a4ff21cbf6d859badf29ec310b34f6496fd
Miguel Bernal <dm.mbernal@gmail.com>
2023-06-29 21:17:29 -0400

    Login formulario

--- myapp/public/js/index.js
+++ myapp/public/js/index.js
@@ -1,3 +1,32 @@
-function login(){
-  console.log('login')
+const usuario = document.getElementById('usuario')
+const clave = document.getElementById('clave')
+
+async function login() {
+  const url = 'api/v1/usuarios/login'
+
+  const data = {
+    usuario: usuario.value,
+    clave: clave.value
+  }
+
+  const parametros = {
+    method: 'POST',
+    headers: {
+      "Content-Type": 'application/json'
+    },
+    body: JSON.stringify(data)
+  }
+
+  const datos = await fetch(url, parametros)
+  const json = await datos.json()
+  console.log(json)
+  if (json.status === 200) {
+    //location.href = 'menu'
+    //localStorage.setItem('token', json.token)
+    localStorage.setItem('id_usuario', json.datos.id)
+    localStorage.setItem('nombre_usuario', json.datos.nombre)
+    localStorage.setItem('usuario_usuario', json.datos.usuario)
+  } else {
+    mensaje('Credencial incorrecta.', 'focus("#usuario")')
+  }
 }
