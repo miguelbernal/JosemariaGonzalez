4df9ef449521a93367256271b820f301ef82b5d5
Miguel Angel Bernal <dm.mbernal@gmail.com>
2023-12-19 20:01:27 -0300

    Usuario router

--- myapp/routes/usuariosRouter.js
+++ myapp/routes/usuariosRouter.js
@@ -7,4 +7,29 @@ router.post('/login', async (req, res, next) => {
     UsuarioController.login(req, res, next)
 })
 
+// Buscar
+router.get('/paginar', function (req, res, next) {
+    UsuarioController.getPage(req, res, next)
+})
+
+// Get One
+router.get('/:id', function (req, res, next) {
+    UsuarioController.getOne(req, res, next)
+})
+
+// Agregar
+router.post('', function (req, res, next) {
+    UsuarioController.insert(req, res, next)
+});
+
+// Modificar
+router.put('/:id', function (req, res, next) {
+    UsuarioController.update(req, res, next)
+});
+
+// Eliminar
+router.delete('/:id', function (req, res, next) {
+    UsuarioController.delete(req, res, next)
+});
+
 module.exports = router
